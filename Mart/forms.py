3899ed5d5a99d144c6e312c61a4f3941f3ca7757
kunalavghade <kunalavghade77@example.com>
2021-11-05 19:11:09 +0530

    changed input fields

--- Mart/forms.py
+++ Mart/forms.py
@@ -4,9 +4,9 @@ from .models import UserInfo
 class Signup(forms.Form):
 	first_name = forms.CharField(max_length = 50,widget=forms.TextInput(attrs={'class':"form-control","placeholder":"Enter first name"}))
 	last_name = forms.CharField(max_length = 50,widget=forms.TextInput(attrs={'class':"form-control","placeholder":"Enter last name"}))
-	email_address = forms.EmailField(max_length = 254,widget=forms.TextInput(attrs={'class':"form-control","placeholder":"Enter Email"}))
-	phone = forms.IntegerField(widget=forms. TextInput(attrs={'class':"form-container","placeholder":"Enter phone"}))
-	password = forms.CharField(max_length = 50,widget=forms.TextInput(attrs={'class':"form-control","placeholder":"Enter Password"}))
+	email_address = forms.EmailField(max_length = 254,widget=forms.EmailInput(attrs={'class':"form-control","placeholder":"Enter Email"}))
+	phone = forms.IntegerField(widget=forms. NumberInput(attrs={'class':"form-container","placeholder":"Enter phone"}))
+	password = forms.CharField(max_length = 50,widget=forms.PasswordInput(attrs={'class':"form-control","placeholder":"Enter Password"}))
 
 class Login(forms.Form):
 	username = forms.CharField(max_length = 100,widget=forms.TextInput(attrs={'class':"form-control","placeholder":"Enter first name"}))
