ec99ee4b24eab1930d7fea67177e00796587a4d3
kunalavghade <kunalavghade77@example.com>
2021-11-12 23:19:32 +0530

    added index page

--- static/index.js
+++ static/index.js
@@ -1,1 +1,8 @@
-"hello"
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
